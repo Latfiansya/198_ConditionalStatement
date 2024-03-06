0801dcd3e5d5f297024a0ff19b245d7a9fc642e0
Latfiansya Dzikri - 198 <160615050+Latfiansya@users.noreply.github.com>
2024-03-06 20:24:02 +0700

    Add files via upload

---
+++ Status kelulusan/Status kelulusan.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int nilaiMatematika, nilaiFisika, nRerata;
+	string status;
+
+	cout << "Masukkan Nilai Matematika : ";
+	cin >> nilaiMatematika;
+	cout << "Masukkan Nilai Fisika : ";
+	cin >> nilaiFisika;
+
+	nRerata = (nilaiMatematika + nilaiFisika) / 2;
+
+	if (nRerata > 60) {
+		status = "Lulus";
+	}
+	else if (nilaiMatematika > 70) {
+		status = "Lulus";
+	}
+	else {
+		status = "Tidak Lulus";
+	}
+
+	cout << "Nilai Matematika anda : " << nilaiMatematika << endl;
+	cout << "Nilai Fisika anda : " << nilaiFisika << endl;
+	cout << "Rata-rata nilai anda : " << nRerata << endl;
+	cout << "Status : " << status << endl;
+	return 0;
+}
